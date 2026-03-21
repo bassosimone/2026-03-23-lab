e851499a0899610627cf348ad5d40bc8c94030a6
Simone Basso <bassosimone@gmail.com>
2026-03-21 15:13:57 +0100

    refactor(host.go): use vflag to parse CLI args

--- internal/server/host.go
+++ internal/server/host.go
@@ -6,23 +6,33 @@ import (
 	"context"
 	"errors"
 	"fmt"
-)
 
-// ErrHostUsage indicates invalid usage of the host command.
-var ErrHostUsage = errors.New("usage: host <domain>")
+	"github.com/bassosimone/vflag"
+)
 
 // runHost implements the "host" command: resolves a domain
 // name and prints the resulting addresses.
 func (s *Simulation) runHost(ctx context.Context, params *RunCommandParams) error {
-	if len(params.Argv) != 2 {
-		err := ErrHostUsage
-		fmt.Fprintf(params.Stderr, "%s\n", err.Error())
+	// Parse flags.
+	fset := vflag.NewFlagSet("host", vflag.ContinueOnError)
+	fset.Stdout = params.Stdout
+	fset.Stderr = params.Stderr
+	fset.AutoHelp('h', "help", "Print this help message and exit.")
+	fset.MinPositionalArgs = 1
+	fset.MaxPositionalArgs = 1
+	if err := fset.Parse(params.Argv[1:]); err != nil {
+		if errors.Is(err, vflag.ErrHelp) {
+			fset.PrintUsageString(params.Stdout)
+			return nil
+		}
+		fset.PrintUsageError(params.Stderr, err)
 		return err
 	}
-	domain := params.Argv[1]
+	domain := fset.Args()[0]
 
 	addrs, err := s.sim.LookupHost(ctx, domain)
 	if err != nil {
+		fmt.Fprintf(params.Stderr, "%s\n", err.Error())
 		return err
 	}
 
